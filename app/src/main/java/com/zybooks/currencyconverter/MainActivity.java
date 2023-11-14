688523273852e5e641f8281dd5150b21eb9dafc2
lennythepenny <lena.sarieva@gmail.com>
2023-11-14 15:02:19 -0700

    Need to add transition from starting screen to MainActivity

--- app/src/main/java/com/zybooks/currencyconverter/MainActivity.java
+++ app/src/main/java/com/zybooks/currencyconverter/MainActivity.java
@@ -6,9 +6,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Locale;
 
@@ -19,65 +24,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText currentCurrencyEditText = findViewById(R.id.currentCurrencyEditText);
-        TextView finalCurrencyTextView = findViewById(R.id.finalCurrencyTextView);
-        Spinner currentCurrencySpinner = findViewById(R.id.currentCurrencySpinner);
-        Spinner finalCurrencySpinner = findViewById(R.id.finalCurrencySpinner);
-        Button calculateButton = findViewById(R.id.calculateButton);
-        Button clearButton = findViewById(R.id.clearButton);
-        Button favoritesButton = findViewById(R.id.favoritesButton);
-        Button swapButton = findViewById(R.id.swapButton);
+        EditText currencyOne = findViewById(R.id.currencyOne);
+        TextView currencyTwo = findViewById(R.id.currencyTwo);
+        //Spinner currentCurrencySpinner = findViewById(R.id.currentCurrencySpinner);
+        //Spinner finalCurrencySpinner = findViewById(R.id.finalCurrencySpinner);
+        Button convertButton = findViewById(R.id.convertButton);
+        Button clearButton2 = findViewById(R.id.clearButton2);
+        //Button favoritesButton = findViewById(R.id.favoritesButton);
+        ImageView swapButton = findViewById(R.id.swapButton);
 
         // Initialize spinners with currency options
         ArrayAdapter<CharSequence> currencyAdapter = ArrayAdapter.createFromResource(this, R.array.currencies_array, android.R.layout.simple_spinner_item);
         currencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        currentCurrencySpinner.setAdapter(currencyAdapter);
-        finalCurrencySpinner.setAdapter(currencyAdapter);
+        //currentCurrencySpinner.setAdapter(currencyAdapter);
+        //finalCurrencySpinner.setAdapter(currencyAdapter);
 
-        currentCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                // Handle current currency selection
-            }
+//        currentCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+//                // Handle current currency selection
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parentView) {
+//                // Do nothing
+//            }
+//        });
+//
+//        finalCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+//                // Handle final currency selection
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parentView) {
+//                // Do nothing
+//            }
+//        });
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // Do nothing
-            }
-        });
-
-        finalCurrencySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                // Handle final currency selection
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parentView) {
-                // Do nothing
-            }
-        });
-
-        calculateButton.setOnClickListener(new View.OnClickListener() {
+        convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Calculate and display currency conversion
             }
         });
 
-        clearButton.setOnClickListener(new View.OnClickListener() {
+        convertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Clear input fields and result
             }
         });
 
-        favoritesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Open the FavoritesActivity
-            }
-        });
+//        favoritesButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Open the FavoritesActivity
+//            }
+//        });
 
         swapButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,5 +90,20 @@ public class MainActivity extends AppCompatActivity {
                 // Swap current and final currencies
             }
         });
+        Toolbar toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
